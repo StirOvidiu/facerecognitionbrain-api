526fb62f763edf69724cf6e412a3bafa9c398aff
Ovidiu <louie.stir@gmail.com>
2020-02-16 20:26:46 +0200

    adding database

--- server.js
+++ server.js
@@ -12,10 +12,8 @@ const image = require('./controllers/image');
 const db = knex({
   client: 'pg',
   connection: {
-    host : 'postgresql-concave-07139',
-    user : 'postgres',
-    password : '',
-    database : 'facerecognitionbrain'
+    host : process.env.DATABASE_URL,
+    ssl: true,
   }
 });
 
